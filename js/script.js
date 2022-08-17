9d2c77ec28e582671f141b41000669e4e983443f
Najma Abdi <anajmabdi@gmail.com>
2022-08-17 11:56:54 -0700

    switch from forEach to for loop

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
   window.addEventListener("load", function(event) {
   event.preventDefault();
 
-  flavors = ['cookie dough', 'chocolate', 'banana split', 'rocky road', 'strawberry'];
-
-  flavors[2] = "vanilla";
+  const flavors = ['cookie dough', 'chocolate', 'banana split', 'rocky road', 'strawberry'];
 
   ul = document.createElement("ul");
 
-  flavors.forEach(function(flavor) {
-    const li = document.createElement("li");
-    li.append(flavor);
-    ul.append(li);
-  });
+  // flavors.forEach(function(flavor) {
+  //   const li = document.createElement("li");
+  //   li.append(flavor);
+  //   ul.append(li);
+  // });
+
+
+for (let i = 0; i < flavors.length; i +=1) {
+  li = document.createElement("li");
+  li.append(flavors[i]);
+  ul.append(li);
+}
 
   const bodyElement = document.querySelector("body");
     bodyElement.append(ul);
